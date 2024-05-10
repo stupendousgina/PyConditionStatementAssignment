97eff5621699f33a48a117fa1551ad9c949d8dd4
stupendousgina <stupendousgina@gmail.com>
2024-05-10 13:27:50 -0500

    third commit

--- Leap Year Explorer.py
+++ Leap Year Explorer.py
@@ -8,7 +8,7 @@
 
 leap_year = int(input("Enter a year to find out if its a leap year: "))
 
-if (leap_year % 400 == 0) and (leap_year % 100 == 0):
+if (leap_year % 400 == 0):
     print(f"{leap_year} is a leap year!")
 elif (leap_year % 4 == 0) and (leap_year % 100 != 0):
     print(f"{leap_year} is a leap year!")
